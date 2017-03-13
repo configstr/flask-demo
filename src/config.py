28a0570561ff94b140b826b62e33147444dcfe65
Jim Kelly <pthread1981@gmail.com>
2017-03-13 14:55:23 +0100

    Add remote config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,31 @@
 import os
+import sys
+import json
 
-WHERE = os.getenv('WHERE', 'Nowhere')
+try:
+    from urllib2 import Request, urlopen
+except ImportError:
+    from urllib.request import Request, urlopen
+
+
+CONFIGSTR_TOKEN = os.getenv('CONFIGSTR_TOKEN')
+CONFIGSTR_URL = os.getenv('CONFIGSTR_URL')
+
+
+if CONFIGSTR_TOKEN and CONFIGSTR_URL:
+    request = Request(CONFIGSTR_URL, headers={
+        'Authorization': 'Bearer {}'.format(CONFIGSTR_TOKEN)
+    })
+else:
+    print(
+        'Not properly configured - please set CONFIGSTR_TOKEN and '
+        'CONFIGSTR_URL in the environment'
+    )
+    sys.exit(1)
+
+
+remote_config = json.loads(
+    urlopen(request).read().decode('utf-8')
+).get('data', {})
+
+WHERE = os.getenv('WHERE', remote_config.get('where'))
